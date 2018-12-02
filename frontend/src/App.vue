90b1b98aacb50b085ebbd6e9252f2269807b115f
Kirill Sudnikovich <0311snitch@gmail.com>
2018-12-02 22:26:34 +0300

    [fix] client id for google auth

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="columns" style="margin-top: 100px;">
+        <div class="column col-2 centered">
+          <!-- Если user - пустой объект, то отображаем кнопку
+          входа через Google -->
+          <g-signin-button
+            v-if="isEmpty(user)"
+            :params="googleSignInParams"
+            @success="onGoogleSignInSuccess"
+            @error="onGoogleSignInError"
+          >
+            <button class="btn btn-block btn-success">
+              Google Signin
+            </button>
+          </g-signin-button>
+          <user-panel v-else :user="user"></user-panel>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+import UserPanel from '@/components/UserPanel'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
-    HelloWorld
+    UserPanel
+  },
+  data () {
+    return {
+      user: {},
+      googleSignInParams: {
+        client_id: '258923662369-g75fs74i6rovf6h8lba9eugc3r4d3edr'
+      }
+    }
+  },
+  methods: {
+    onGoogleSignInSuccess (resp) {
+      const token = resp.Zi.access_token
+      // После успешного входа через Google,
+      // отправляем токен доступа на бэкэнд и получаем взамен
+      // пользователя и JWT токен
+      // P.S. JWT токен в нашем примере не нужен, поэтому его не сохраняем
+      axios.post('http://localhost:8000/auth/google/', {
+        access_token: token
+      })
+        .then(resp => {
+          this.user = resp.data.user
+        })
+        .catch(err => {
+          console.log(err.response)
+        })
+    },
+    onGoogleSignInError (error) {
+      console.log('OH NOES', error)
+    },
+    isEmpty (obj) {
+      return Object.keys(obj).length === 0
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
