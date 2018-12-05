cd5341f9a058689dc4c074e6283c63c466bd9744
Kirill Sudnikovich <0311snitch@gmail.com>
2018-12-05 16:39:37 +0300

    [fix] webpack app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,67 +2,62 @@
   <div id="app">
     <div class="container">
       <div class="columns" style="margin-top: 100px;">
+        <div class="col-5"></div>
         <div class="column col-2 centered">
           <!-- Если user - пустой объект, то отображаем кнопку
-          входа через Google -->
+          входа через Google-->
           <g-signin-button
             v-if="isEmpty(user)"
             :params="googleSignInParams"
             @success="onGoogleSignInSuccess"
             @error="onGoogleSignInError"
           >
-            <button class="btn btn-block btn-success">
-              Google Signin
-            </button>
+            <button class="btn btn-block btn-success">Google Signin</button>
           </g-signin-button>
           <user-panel v-else :user="user"></user-panel>
         </div>
+        <div class="col-5"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
-import UserPanel from '@/components/UserPanel'
+import UserPanel from "@/components/UserPanel";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     UserPanel
   },
-  data () {
+  data() {
     return {
       user: {},
       googleSignInParams: {
-        client_id: '258923662369-g75fs74i6rovf6h8lba9eugc3r4d3edr'
+        client_id: "258923662369-g75fs74i6rovf6h8lba9eugc3r4d3edr"
       }
-    }
+    };
   },
   methods: {
-    onGoogleSignInSuccess (resp) {
-      const token = resp.Zi.access_token
+    onGoogleSignInSuccess(resp) {
+      const token = resp.Zi.access_token;
       // После успешного входа через Google,
       // отправляем токен доступа на бэкэнд и получаем взамен
       // пользователя и JWT токен
       // P.S. JWT токен в нашем примере не нужен, поэтому его не сохраняем
-      axios.post('http://localhost:8000/auth/google/', {
-        access_token: token
-      })
+      axios
+        .post("http://localhost:8000/auth/google/", {
+          access_token: token
+        })
         .then(resp => {
-          this.user = resp.data.user
-        })
-        .catch(err => {
-          console.log(err.response)
-        })
+          this.user = resp.data.user;
+        });
     },
-    onGoogleSignInError (error) {
-      console.log('OH NOES', error)
-    },
-    isEmpty (obj) {
-      return Object.keys(obj).length === 0
+    isEmpty(obj) {
+      return Object.keys(obj).length === 0;
     }
   }
-}
+};
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import GSignInButton from 'vue-google-signin-button'
-import App from './App'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
 Vue.use(GSignInButton)
 
-/* eslint-disable no-new */
+
+Vue.config.productionTip = false
+
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App),
+}).$mount('#app')
